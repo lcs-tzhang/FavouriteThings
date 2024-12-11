715e156645d490dacad1e4f116637c32692cf51a
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-11 11:32:26 -0500

    Initial Commit

---
+++ FavouriteThings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FavouriteThings
+//
+//  Created by xinyu zhang on 2024-12-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by xinyu zhang on 2024-12-11.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
